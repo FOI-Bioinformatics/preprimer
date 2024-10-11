6f0af92bbd4fb6e1ef3bb1550df579f84c378c95
CarolineOhrman <caroline.ohrman@foi.se>
2024-10-11 12:20:32 +0200

    Major bug fixes

--- preprimer/utils.py
+++ preprimer/utils.py
@@ -1,9 +1,16 @@
+import sys
 import os
 import shutil
 import re
 import tempfile
 import subprocess
 from pathlib import Path
+
+##
+## Contains three classes:
+## - FileHandler
+## - AmpliconUpdater
+## - Aligners
 
 class FileHandler:
     @staticmethod
@@ -23,7 +30,7 @@ class FileHandler:
             return True
 
     @staticmethod
-    def check_folder_presence(directory, force=False):
+    def check_folder_exists(directory, force=False):
         if not os.path.exists(directory):
             Path(directory).mkdir(parents=True, exist_ok=True)
             print(f"Output folder {directory} created successfully!")
@@ -35,79 +42,15 @@ class FileHandler:
                 print(f"Output folder {directory} created successfully!")
                 return True
             else:
+                print("end")
                 return False
-
-    @staticmethod
-    def search_missing_files(files):
-        missing_files = []
-        for file in files:
-            if not os.path.isfile(file):
-                missing_files.append(file)
-        if missing_files:
-            print(f"The following required files are missing:")
-            for filename in missing_files:
-                print(filename)
-            return False
-        return True
 
     @staticmethod
     def copy_file(src, dst):
         shutil.copy(src, dst)
 
-class PrimerWriter:
-    @staticmethod
-    def write_sts(file_path, amplicons, reference):
-        with open(file_path, 'w') as output_file:
-            for key in amplicons:
-                amplicon_name = key
-                seq_fw = ''
-                seq_rw = ''
-                amplicon_len = ''
-                for primer in amplicons[amplicon_name]:
-                    if primer['direction'] == 'forward':
-                        seq_fw = primer['seq']
-                    if primer['direction'] == 'reverse':
-                        seq_rw = primer['seq']
-                    amplicon_len = primer['amplicon_length']
-                output_file.write(f"{amplicon_name}\t{seq_fw}\t{seq_rw}\t{amplicon_len}\n")
-
-    @staticmethod
-    def write_fasta(file_path, amplicon_dict):
-        with open(file_path, 'w') as output_file:
-            for key in amplicon_dict:
-                for primer in amplicon_dict[key]:
-                    output_file.write(f">{primer['artic_primer_name']}\n{primer['seq']}\n")
-
-    @staticmethod
-    def write_artic_scheme(file_path, amplicon_dict):
-        with open(file_path, 'w') as output_file:
-            for amplicon_name in amplicon_dict:
-                for primer in amplicon_dict[amplicon_name]:
-                    output_file.write(f"{primer['reference_id']}\t{primer['start']}\t{primer['stop']}\t{primer['artic_primer_name']}\t{primer['pool']}\t{primer['strand']}\n")
-
-    @staticmethod
-    def run_exonerate(id, output_path, sequence, reference_genome):
-        with tempfile.NamedTemporaryFile(mode='w', delete=False) as seq_file:
-            seq_file.write(f">{id}\n" + sequence)
-            seq_file_path = seq_file.name
-
-        exonerate_command = [
-            "exonerate",
-            "--model", "affine:local",
-            "--query", seq_file_path,
-            "--target", reference_genome,
-            "--showalignment", "TRUE",
-            "--showvulgar", "FALSE",
-            "--showcigar", "TRUE",
-            "--ryo", "%qas",
-            "--percent", "90",
-            "--bestn", "1"
-        ]
-        output_file_path = f"{output_path}/{id}.aln"
-        with open(output_file_path, 'w') as output_file:
-            subprocess.run(exonerate_command, stdout=output_file, text=True)
-        return output_file_path
-
+#___________________________________________________________
+#___________________________________________________________
 class AmpliconUpdater:
     @staticmethod
     def find_closest_pair(fw_primer_list, rw_primer_list, old_amplicon_length):
@@ -132,53 +75,59 @@ class AmpliconUpdater:
         return forward, reverse, length
 
     @staticmethod
-    def translate_amplicon_info_to_new_reference(amplicon_info, reference_fasta, output_dir):
+    def translate_amplicon_info_to_new_reference(amplicon_info, reference_fasta, output_dir, force=False):
         updated_amplicon_info = {}
-        
         for amplicon_name, primers in amplicon_info.items():
             old_forward_primer, old_reverse_primer = primers
             old_length = old_forward_primer['amplicon_length']
+            alignments = []
+            exclude_amplicon = []
 
-            alignments = [
-                PrimerWriter.run_exonerate(
-                    primer['primer_name'], output_dir, primer['seq'], reference_fasta
-                )
-                for primer in primers
-            ]
+            for primer in primers:
+                primer_name = primer['primer_name']
+                primer_seq = primer['seq']
+                
+                exonerate_output = Aligner.run_exonerate(primer_name, output_dir, primer_seq, reference_fasta)
+                parsed_alignment = Aligner.parse_exonerate_output(exonerate_output)
+                
+                # If parsing exonerate output fails i.e. no alignments found
+                if not parsed_alignment:
+                    print(f"\nNo alignment found for primer {primer_name} in amplicon {amplicon_name} to {reference_fasta}.")
+                    if not force:
+                        user_input = input(f"Do you want to continue anyway?  (y/n): ")
+                        if user_input.lower() == 'n':
+                            print(f"Program will exit. Try a different reference")
+                            exit()
+                    else: 
+                        print("Option --force excludes by default.")
+                    print(f"Amplicon named \"{amplicon_name}\" excluded.\n")
+                elif parsed_alignment:
+                    alignments.append(parsed_alignment)
 
-            forward_primer, reverse_primer, amplicon_length = AmpliconUpdater.find_closest_pair(alignments[0], alignments[1], old_length)
+            #A succefull alignment where both primers have have one ore more hits
+            if len(alignments) == 2:
+                forward_parsed, reverse_parsed = alignments
+                if forward_parsed and reverse_parsed:
+                    forward_primer, reverse_primer, amplicon_length = AmpliconUpdater.find_closest_pair(forward_parsed, reverse_parsed, old_length)
+                    updated_primers = []
+                    for old_primer, new_primer in zip(primers, [forward_primer, reverse_primer]):
+                        updated_primer = old_primer.copy()
+                        for key in old_primer.keys():
+                            if key in new_primer:
+                                updated_primer[key] = new_primer[key]
+                        updated_primer['strand'] = '+' if updated_primer['start'] < updated_primer['stop'] else '-'
+                        updated_primer['amplicon_length'] = amplicon_length
+                        updated_primers.append(updated_primer)
+                    
+                    if updated_primers:
+                        updated_amplicon_info[amplicon_name] = updated_primers
+            elif len(alignments) == 0:
+                continue
 
-            updated_primers = []
-            for old_primer, new_primer in zip(primers, [forward_primer, reverse_primer]):
-                updated_primer = old_primer.copy()
-                for key in old_primer.keys():
-                    if key in new_primer:
-                        updated_primer[key] = new_primer[key]
-                updated_primer['strand'] = '+' if updated_primer['start'] < updated_primer['stop'] else '-'
-                updated_primer['amplicon_length'] = amplicon_length
-                updated_primers.append(updated_primer)
-            updated_amplicon_info[amplicon_name] = updated_primers
         return updated_amplicon_info
 
-        @staticmethod
-        def run_me_pcr(primer_sts_file, reference_genome,  use_temp_file, regular_filepath):
-            if use_temp_file:
-                # Create a temporary file in the specified folder
-                with tempfile.NamedTemporaryFile(mode='w', delete=False) as output_file:
-                    output_file_path = output_file.name
-            else:
-                # Create a regular file in the specified folder
-                if regular_filepath is None:
-                    raise ValueError("A regular filename must be provided if not using a temporary file.")
-                output_file_path = regular_filepath
-
-            # Define me-PCR command
-            me_pcr_command = f"me-PCR {primer_sts_file} {reference_genome} O={output_file_path} M=1000"
-            # Run me-PCR command
-            subprocess.run(me_pcr_command, shell=True)
-
-            return output_file_path
-
+#___________________________________________________________
+#___________________________________________________________
 class Aligner:
     @staticmethod
     def run_me_pcr(primer_sts_file, reference_genome,  use_temp_file, regular_filepath):
@@ -198,3 +147,66 @@ class Aligner:
         subprocess.run(me_pcr_command, shell=True)
 
         return output_file_path
+
+    @staticmethod
+    def run_exonerate(id, output_path, sequence, reference_genome):
+        with tempfile.NamedTemporaryFile(mode='w', delete=False) as seq_file:
+            seq_file.write(f">{id}\n" + sequence)
+            seq_file_path = seq_file.name
+       
+        exonerate_command = [
+            "exonerate",
+            "--model", "affine:local",
+            "--query", seq_file_path,
+            "--target", reference_genome,
+            "--showalignment", "TRUE",
+            "--showvulgar", "FALSE",
+            "--showcigar", "TRUE",
+            "--ryo", "%qas",
+            "--percent", "90",
+            "--bestn", "1"
+        ]
+        output_file_path = f"{output_path}/{id}.aln"
+        with open(output_file_path, 'w') as output_file:
+            subprocess.run(exonerate_command, stdout=output_file, text=True)
+        return output_file_path
+
+    @staticmethod
+    def parse_exonerate_output(output_file_path):
+        alignments = []
+        alignment = {}
+        
+        with open(output_file_path, 'r') as file:
+            lines = file.readlines()
+        
+        for line in lines:
+            line = line.strip()
+            
+            if line.startswith('cigar:'):
+                # Process cigar line and add alignment to the list
+                cigar_parts = line.split()
+                if len(cigar_parts) >= 8:
+                    alignment['primer_name'] = cigar_parts[1]
+                    alignment['query_start'] = int(cigar_parts[2])
+                    alignment['query_end'] = int(cigar_parts[3])
+                    alignment['reference_id'] = cigar_parts[5]
+                    alignment['start'] = int(cigar_parts[6])
+                    alignment['stop'] = int(cigar_parts[7])
+                    alignment['strand'] = cigar_parts[8]
+                    alignment['score'] = float(cigar_parts[9])
+                    alignment['cigar'] = f'{cigar_parts[10]}{cigar_parts[11]}'
+                    
+                    # Add alignment to list if all fields are present
+                    if all(key in alignment for key in ['query_start', 'query_end', 'start', 'stop', 'score']):
+                        alignments.append(alignment)
+                        alignment = {}
+        return alignments
+
+    @staticmethod
+    def contains_non_atgc(string):
+        # Define a regular expression pattern to match any character except 'a', 't', 'c', and 'g'
+        pattern = re.compile(r'[^atcg]', re.IGNORECASE)
+        # Search for any non-ATCG characters in the string
+        non_atgc_chars = pattern.findall(string)
+        # If a match is found, return True (indicating presence of non-ATCG characters), otherwise return False
+        return non_atgc_chars
---
+++ preprimer/writers.py
@@ -0,0 +1,81 @@
+import os
+import shutil
+import re
+import tempfile
+import subprocess
+from pathlib import Path
+from preprimer.utils import Aligner
+
+def write_sts(file_path, amplicons, reference):
+        with open(file_path, 'w') as output_file:
+            for key in amplicons:
+                amplicon_name = key
+                seq_fw = ''
+                seq_rw = ''
+                amplicon_len = ''
+                for primer in amplicons[amplicon_name]:
+                    if primer['direction'] == 'forward':
+                        seq_fw = primer['seq']
+                    if primer['direction'] == 'reverse':
+                        seq_rw = primer['seq']
+                    amplicon_len = primer['amplicon_length']
+                output_file.write(f"{amplicon_name}\t{seq_fw}\t{seq_rw}\t{amplicon_len}\n")
+
+# Write sts output format.
+def write_sts_2(file_path, amplicon_info, reference):
+    #sts.file
+    """
+    amplicon_0      actgctgtaggcgtcaaagatt  cggaaataatacggtgggcgaga 2737
+    amplicon_1      tcctcatgcgaattcactccca  cgaacagaatgcccacaacaca  2914
+    amplicon_2      aattggtaggggcggtygtga   tggactgcgcaaatccaacatc  2571
+    amplicon_3      atgccaccgggaaactgtaca   gctgctttgtatgtgcgctttg  2884
+    """
+
+    with open(file_path, 'w') as output_file:
+        # output_file.write("ID\tSNP-pos\tSNP-state\tForward\tReverse\n")
+        for key in amplicon_info:
+        # for i, (fw, rw) in enumerate(pairs):
+            amplicon_name = key
+            seq_fw = ''
+            seq_rw = ''
+            len = ''
+            for primer in amplicon_info[amplicon_name]:
+                primer_name = primer['primer_name']
+                amplicon_len = primer['amplicon_length']
+                seq = primer['seq']
+                if primer['direction'] == 'forward':
+                    seq_fw = seq
+                if primer['direction'] == 'reverse':
+                    seq_rw = seq
+                amplicon_len = primer['amplicon_length']
+
+                if Aligner.contains_non_atgc(seq):
+                    aln_path = Aligner.run_exonerate(primer_name, os.path.dirname(file_path), seq, reference)                   
+                    with open(aln_path, 'r') as file:
+                        for line in file:
+                            print(line, end='')
+                        print(f"WARNING! You are creating an sts with a primer with non ATCG characters")
+                        print(f'WARNING! Ambigous bases do not work in me-pcr.')
+                        print(f'WARNING! Consider changing the primer of amplicon: {amplicon_name}, primer: {primer_name}, sequence: {seq} to atgc characters {Aligner.contains_non_atgc(seq)}')
+                        print(f'WARNING! before using the sts')
+                        print(f'WARNING! Alignment in {aln_path}.')
+                        response = input(f"Do you want to continue with ambigous bases in the primers? (y/n): ").strip().lower()
+                        if response == 'n':
+                            print("Aborted. Change the input primers and try again!")
+                            sys.exit(1)
+                        else: 
+                            pass
+
+            output_file.write(f"{amplicon_name}\t{seq_fw}\t{seq_rw}\t{amplicon_len}\n")
+
+def write_fasta(file_path, amplicon_dict):
+    with open(file_path, 'w') as output_file:
+        for key in amplicon_dict:
+            for primer in amplicon_dict[key]:
+                output_file.write(f">{primer['artic_primer_name']}\n{primer['seq']}\n")
+
+def write_artic(file_path, amplicon_dict):
+    with open(file_path, 'w') as output_file:
+        for amplicon_name in amplicon_dict:
+            for primer in amplicon_dict[amplicon_name]:
+                output_file.write(f"{primer['reference_id']}\t{primer['start']}\t{primer['stop']}\t{primer['artic_primer_name']}\t{primer['pool']}\t{primer['strand']}\n")
